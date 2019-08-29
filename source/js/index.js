4f84dc55fde355a9564cdac605bcfc25fb030123
nessun <7planeta@bk.ru>
2019-08-29 16:56:29 +0300

    correction of deficiencies

--- source/js/index.js
+++ source/js/index.js
@@ -1,11 +1,19 @@
-//= _polyfills/picture.js
-
-var page = document.querySelector('.page');
 var menu = document.querySelector('.menu');
 var menuBtn = document.querySelector('.menu__button');
+var productLinksPicture = document.querySelectorAll('.product__link-picture');
+var productLinks = document.querySelectorAll('.product__link');
+var orderProductBtn = document.querySelectorAll('.product__button');
 
 function removeJsClass(el, className) {
   el.classList.remove(className);
+}
+
+function openProductModal(links) {
+  for (var i=0; i < links.length; i++) {
+    links[i].addEventListener('click', function (event) {
+      event.preventDefault();
+    })
+  }
 }
 
 function toggleMenu(el) {
@@ -25,10 +33,13 @@ function yandexMap() {
     var myMap;
 
     myMap = new ymaps.Map('map', {
-      center: (screen.width < 1300) ? ([59.93877121663107, 30.323274149999975]) : ([59.939075, 30.32095]),
-      zoom: 18,
+      center: (window.innerWidth >= 1300) ? ([59.938950, 30.31965]) : (window.innerWidth > 767 && window.innerWidth < 1300) ? ([59.93897121663107, 30.323274149999975]) : ([59.93870121663107, 30.323274149999975]),
+      zoom: 17,
       controls: []
-    });
+    },
+      {
+        autoFitToViewport: 'always'
+      });
 
     myMap.behaviors.disable('scrollZoom');
 
@@ -36,23 +47,24 @@ function yandexMap() {
       position: {top: 100, right: 25}
     });
 
-    var myPlacemark = new  ymaps.Placemark([59.93877121663107, 30.323274149999975] , {},
+    var myPlacemark = new ymaps.Placemark([59.93877121663107, 30.323274149999975] , {},
       {
         iconLayout: 'default#image',
         iconImageHref: 'img/map-pin.png',
-        iconImageSize: (screen.width > 767) ? [124, 106] : [62, 53],
-        iconImageOffset: (screen.width > 767) ? [-62, -83] : [-31, -27.5]
+        iconImageSize: (window.innerWidth > 767) ? [124, 106] : [62, 53],
+        iconImageOffset: (window.innerWidth > 767) ? [-62, -83] : [-31, -27.5]
       });
     myMap.geoObjects.add(myPlacemark);
   }
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  removeJsClass(page, 'page__no-js');
+  removeJsClass(menuBtn, 'menu__button--no-js');
   toggleMenu(menu);
   yandexMap();
+  if (productLinks && productLinksPicture && orderProductBtn) {
+    openProductModal(productLinks);
+    openProductModal(productLinksPicture);
+    openProductModal(orderProductBtn);
+  }
 });
-
-
-
-
